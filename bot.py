8a089d827eef9d3b2e62477621183eb58b11c645
Diogo Soares <dsmozh@gmail.com>
2021-11-09 01:00:54 -0400

    add main code

---
+++ bot.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from secrets import *
+import tweepy
+from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import time
+
+def get_from_csv():
+	'''
+		@TODO
+			- read file from github page
+	'''
+	df = pd.read_csv("input.csv",sep=';',usecols= ['user','comida'])
+	matrix = df.apply(lambda x: x.dropna().tolist()).tolist()
+	
+	return (np.random.choice(matrix[0]), np.random.choice(matrix[1]))
+
+def create_tweet():
+	user, comida = get_from_csv()
+	print(user,comida)
+	tweet = """
+            Já convidou o {} para comer um ~{} no @TeComoEmCasa hoje?
+            """.format(user, comida)
+	return tweet
+	
+def tweet_send():
+	intervalo = 60 * 60 * 24 #roda a cada 24h
+	auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+	auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+	api = tweepy.API(auth)
+	
+	while True:
+		print("tweetandoo...")
+		tweet_txt = create_tweet()
+		api.update_status(tweet_txt)
+		time.sleep(intervalo)
+
+if __name__ == "__main__" :
+	tweet_send()
---
+++ secrets.py
@@ -0,0 +1,6 @@
+from os import environ
+C_KEY = environ['CONSUMER_KEY']
+C_SECRET = environ['CONSUMER_SECRET']
+A_TOKEN = environ['ACCESS_KEY']
+A_TOKEN_SECRET = environ['ACCESS_SECRET']
+
